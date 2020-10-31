d2ba65c25a8b48dcf714958fe169811e48e2603f
Gonzalo Waack Carneado <alum.gwaackc.2020@iesalixar.org>
2020-10-31 18:47:02 +0100

    rename files

--- U2.Programacion_estudiada_en_Java/Libro2/Ejercicio26_Ej2_Pag23.java
+++
@@ -1,9 +0,0 @@
-package com.Libro2;
-
-public class Ejercicio26_Ej2_Pag23 {
-    public static void main(String[] args) {
-        String nombre="Gonzalo Waack carneado";
-
-        System.out.println("Nombre: "+nombre);
-    }
-}
--- U2.Programacion_estudiada_en_Java/Libro2/Ejercicio26_Ej7_Pag23.java
+++
@@ -1,8 +0,0 @@
-package com.Libro2;
-
-public class Ejercicio26_Ej7_Pag23 {
-    public static void main(String[] args) {
-        char g=71, o=111,n=110,z=122,a=97,l=108,e=101, s=115;
-        System.out.println(g+""+o+""+n+""+z+""+a+""+l+""+e+""+s);
-    }
-}
