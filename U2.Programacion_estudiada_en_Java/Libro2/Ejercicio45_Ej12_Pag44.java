2d63b2ba19ed9b039f70cc0c6f2d8dc46caa3574
Gonzalo Waack Carneado <alum.gwaackc.2020@iesalixar.org>
2020-10-31 18:16:13 +0100

    Ejercicios Libro Ampliación

---
+++ U2.Programacion_estudiada_en_Java/Libro2/Ejercicio45_Ej12_Pag44.java
@@ -0,0 +1,134 @@
+package com.Libro2;
+
+import java.util.Scanner;
+
+public class Ejercicio45_Ej12_Pag44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean correcta=false;
+        int acertadas=0;
+        System.out.println("Pregunta1.?");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+        int respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta2.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta3.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+ 1: acertadas;
+        System.out.println("Pregunta4.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta5.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta6.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta7.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta8.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta9.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+        System.out.println("Pregunta10.");
+        System.out.println("1 : Respuesta");
+        System.out.println("2 : Respuesta");
+        System.out.println("3 : Respuesta");
+            respuesta = sc.nextInt();
+
+        switch (respuesta){
+            case 1: correcta=true;break;
+            case 2: correcta=false;break;
+            case 3: correcta=false;break;
+        }
+        acertadas = (correcta ==true) ? acertadas+1 : acertadas;
+
+        System.out.println("Has sacado un "+acertadas+"/10");
+    }
+}
---
+++ U2.Programacion_estudiada_en_Java/Libro2/Ejercicio45_Ej14_Pag44.java
@@ -0,0 +1,22 @@
+package com.Libro2;
+
+import java.util.Scanner;
+
+public class Ejercicio45_Ej14_Pag44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Introduce un numero: ");
+        int n = sc.nextInt();
+
+        if(n%2==0 && n%5==0){
+            System.out.println("El numero "+n+" es par y divisible por 5");
+        }else if(n%2==0){
+            System.out.println("El numero "+n+" es par");
+        }else if(n%5==0){
+            System.out.println("El numero "+n+" es divisible por 5");
+        }else{
+            System.out.println("El numero es impar y no divisible por 5");
+        }
+    }
+}
